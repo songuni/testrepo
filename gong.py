2b74f672e253f495feb549cc253b5d59634f715d
uni <97011747+songuni@users.noreply.github.com>
2022-01-13 14:59:31 +0900

    Delete gong.py

--- gong.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-filename = '//wsl$/Ubuntu/home/uni/going/testrepo/single_crystal_test_out.csv'
-data = np.genfromtxt(filename,dtype=float,delimiter=',',names=True)
-print(data['time'])
